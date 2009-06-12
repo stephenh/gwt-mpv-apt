f514c018a9d10ee26fb02665a657ca13cb3d8a2e
Stephen Haberman <stephen@exigencecorp.com>
2009-06-11 21:47:39 -0500

    Rename constant gwtAnnotation -> gwtAnnotationClassName.

--- src/org/exigencecorp/gwtasyncgen/Processor.java
+++ src/org/exigencecorp/gwtasyncgen/Processor.java
@@ -11,15 +11,15 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic.Kind;
 
-@SupportedAnnotationTypes( { Processor.gwtAnnotation })
+@SupportedAnnotationTypes( { Processor.gwtAnnotationClassName })
 @SupportedSourceVersion(SourceVersion.RELEASE_6)
 public class Processor extends AbstractProcessor {
 
-    public static final String gwtAnnotation = "com.google.gwt.user.client.rpc.RemoteServiceRelativePath";
+    private static final String gwtAnnotationClassName = "com.google.gwt.user.client.rpc.RemoteServiceRelativePath";
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        TypeElement annotation = this.processingEnv.getElementUtils().getTypeElement(gwtAnnotation);
+        TypeElement annotation = this.processingEnv.getElementUtils().getTypeElement(gwtAnnotationClassName);
         for (Element element : roundEnv.getElementsAnnotatedWith(annotation)) {
             if (element instanceof TypeElement) {
                 new Generator(this.processingEnv, (TypeElement) element).generate();
