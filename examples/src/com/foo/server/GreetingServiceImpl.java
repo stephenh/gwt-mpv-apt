62d583c706de9cd4f07918f2bce0febf27765a99
Stephen Haberman <stephen@exigencecorp.com>
2009-06-18 09:55:55 -0500

    Use the right example--not sure if I'm supposed to check in the GWT generated stuff or not.

---
+++ examples/src/com/foo/server/GreetingServiceImpl.java
@@ -0,0 +1,26 @@
+package com.foo.server;
+
+import com.foo.client.GreetingService;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * The server side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class GreetingServiceImpl extends RemoteServiceServlet implements GreetingService {
+
+	public String greetServer(String input) {
+		String serverInfo = getServletContext().getServerInfo();
+		String userAgent = getThreadLocalRequest().getHeader("User-Agent");
+		return "Hello, " + input + "!<br><br>I am running " + serverInfo
+				+ ".<br><br>It looks like you are using:<br>" + userAgent;
+	}
+
+	public boolean didGreet() {
+		return true;
+	}
+
+	public void fooGreat(String one, int two) {
+	}
+	
+}
