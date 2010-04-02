6080e4e1dc2513c42493f7708ab1907ccdfc9214
Stephen Haberman <stephen@exigencecorp.com>
2010-04-02 16:03:00 -0500

    Add GenStub.NAME.

--- processor/src/org/gwtasyncgen/GenStub.java
+++ processor/src/org/gwtasyncgen/GenStub.java
@@ -1,6 +1,9 @@
 package org.gwtasyncgen;
 
 public @interface GenStub {
+	public static final String NAME = "org.gwtasyncgen.GenStub";
+
 	String name() default "";
+
 	boolean isAbstract() default false;
 }
