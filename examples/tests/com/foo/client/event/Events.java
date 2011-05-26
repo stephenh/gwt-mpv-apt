466613c97288b4f0fbb9359ba41b36420b051ecb
Stephen Haberman <stephen@exigencecorp.com>
2011-05-26 12:59:51 -0500

    Update examples for both bindery and old school events.

--- examples/tests/com/foo/client/event/Events.java
+++ examples/tests/com/foo/client/event/Events.java
@@ -1,6 +1,8 @@
 package com.foo.client.event;
 
 import com.google.gwt.event.shared.HandlerManager;
+import com.google.web.bindery.event.shared.EventBus;
+import com.google.web.bindery.event.shared.SimpleEventBus;
 
 public class Events {
 
@@ -17,8 +19,8 @@ public class Events {
 	}
 
 	public static void main(String[] args) {
-		HandlerManager t = new HandlerManager(null);
-		t.addHandler(GenericChangedEvent.getType(), new GenericChangedHandler<String>() {
+		EventBus b = new SimpleEventBus();
+		b.addHandler(GenericChangedEvent.getType(), new GenericChangedHandler<String>() {
 			@Override
 			public void onGenericChanged(GenericChangedEvent<String> event) {
 				System.out.println(event.getT());
@@ -32,15 +34,20 @@ public class Events {
 		System.out.println(new BarChangedEvent(1).equals(new BarChangedEvent(1)));
 		System.out.println(new BarChangedEvent(1).equals(new BarChangedEvent(2)));
 
-		HandlerManager manager = new HandlerManager(null);
-		BarChangedEvent.fire(manager, 1);
-		BoundsChangedEvent.fire(manager, 1, 2);
+		BarChangedEvent.fire(b, 1);
+		BoundsChangedEvent.fire(b, 1, 2);
 
 		// EventBus bus = new DefaultEventBus();
 		// BarChangedEvent.fire(bus, 1);
 		// BoundsChangedEvent.fire(bus, 1, 2);
 
-		t.fireEvent(new GenericChangedEvent<String>("foo"));
+		b.fireEvent(new GenericChangedEvent<String>("foo"));
 		// t.fireEvent(new GenericChangedEvent<Integer>(1));
+
+    // setting gwtEvent = true allows using the old GwtEvent
+		HandlerManager h = new HandlerManager(null);
+		OldSchoolEvent.fire(h, 1);
+		com.google.gwt.event.shared.EventBus o = new com.google.gwt.event.shared.SimpleEventBus();
+		OldSchoolEvent.fire(o, 1);
 	}
 }
